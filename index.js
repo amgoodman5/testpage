34a7b0a2a0e8490feaabc47f61fa32a83ffe71fb
Aaron Goodman <amgoodman5@gmail.com>
2018-02-05 20:28:01 -0700

    most functionality

---
+++ index.js
@@ -0,0 +1,37 @@
+
+$(() => {
+
+playVideo();
+alertEmailValue();
+
+var text = `Intro To Process of Awakening`
+
+function playVideo(){
+  let video = ".video";
+  let playicon = ".play";
+  let videoText ='.video-text';
+  $('.video').parent().click(function () {
+    if($(this).children(video).get(0).paused){
+        $(this).children(video).get(0).play();
+        $(this).children(playicon).fadeOut();
+    }else{
+       $(this).children(video).get(0).pause();
+        $(this).children(playicon).fadeIn();
+    }
+  });
+}
+
+function alertEmailValue(){
+  $( ".button--submit" ).click(function(event) {
+    event.preventDefault();
+      let emailvalue = $(this).closest("form").find("input[name='emailbox']").val();
+      alert(emailvalue );
+      return false;
+  });
+}
+
+
+
+
+
+});
